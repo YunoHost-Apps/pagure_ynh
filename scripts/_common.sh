7dfe48267c2cd7ee8f4aa97872ba17e373e4d28a
Félix Piédallu <felix@piedallu.me>
2024-03-15 23:27:46 +0100

    Manifest v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-#pkg_dependencies="git python-dev libgit2-dev redis-server libjpeg-dev gcc libffi-dev virtualenv python-virtualenv python-cffi python-gdbm python-psycopg2 postgresql uwsgi uwsgi-plugin-python"
-pkg_dependencies="git python3 python3-dev libgit2-dev redis-server libjpeg-dev gcc libffi-dev python3-venv python3-cffi python3-gdbm python3-psycopg2 libpq-dev postgresql"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
